499c254f210b309a31bbc15fbc28894a53786566
jihoon <wlgnstls0413@naver.com>
2026-07-07 11:38:32 +0900

    feat: 임시 레이어드 아키텍처 생성

---
+++ payment-service/src/main/java/com/bubbletea/payment/repository/PaymentMethodRepository.java
@@ -0,0 +1,11 @@
+package com.bubbletea.payment.repository;
+
+import com.bubbletea.payment.entity.PaymentMethod;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Long> {
+    List<PaymentMethod> findAllByUserId(Long userId);
+}
