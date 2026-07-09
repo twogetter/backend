8a154a861b35416f85e1bcece8fde151323cb8d8
muncool39 <muncool39@gmail.com>
2026-07-09 10:40:49 +0900

    feat: 예외 정의 및 공통 핸들러 추가

---
+++ common-lib/src/main/java/com/bubbletea/common/exception/AppException.java
@@ -0,0 +1,20 @@
+package com.bubbletea.common.exception;
+
+
+import lombok.Getter;
+
+@Getter
+public class AppException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public AppException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public AppException(ErrorCode errorCode, String message) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+}
