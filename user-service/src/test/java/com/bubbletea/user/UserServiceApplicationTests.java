76e6a36480be9ee45ea73e2a5163c4156ddb7682
유창민 <dnwn3295@gmail.com>
2026-07-14 02:37:39 +0900

    test(user): 애플리케이션 컨텍스트 테스트 복구

--- user-service/src/test/java/com/bubbletea/user/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/bubbletea/user/UserServiceApplicationTests.java
@@ -1,10 +1,16 @@
 package com.bubbletea.user;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
+@ActiveProfiles("test")
 class UserServiceApplicationTests {
 
     @Test
+    @DisplayName("User Service 애플리케이션 컨텍스트를 정상적으로 로드한다")
     void contextLoads() {
     }
 }
