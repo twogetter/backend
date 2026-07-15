fd02b0d9eed48d03d8bddabc261771cb248fcbd2
jihoon <wlgnstls0413@naver.com>
2026-07-15 10:38:59 +0900

    feat: 프론트 관련

--- payment-service/src/main/java/com/bubbletea/payment/controller/web/ViewController.java
+++ payment-service/src/main/java/com/bubbletea/payment/controller/web/ViewController.java
@@ -19,6 +19,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -93,7 +94,7 @@ public class ViewController {
         return "widget/index";
     }
 
-    @RequestMapping(value = {"/confirm/widget", "/confirm/payment"})
+    @PostMapping(value = {"/confirm/widget", "/confirm/payment"})
     public ResponseEntity<JSONObject> confirmPayment(HttpServletRequest request, @RequestBody String jsonBody) throws Exception {
         String secretKey = request.getRequestURI().contains("/confirm/payment") ? API_SECRET_KEY : WIDGET_SECRET_KEY;
         JSONObject response = sendRequest(parseRequestData(jsonBody), secretKey, "https://api.tosspayments.com/v1/payments/confirm");
