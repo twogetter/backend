be4da3a48def48755adc6ae5f235c261aa0a7631
muncool39 <muncool39@gmail.com>
2026-07-20 15:43:45 +0900

    build: 테스트를 위한 common-test 모듈 추가 및 의존성 설정
    - build.gradle에 bom 추가
    - setting.gradle에 common-test 모듈 등록
    - common-test/build.gradle 작성

---
+++ common-test/src/main/java/com/bubbletea/commontest/CommonTestApplication.java
@@ -0,0 +1,13 @@
+package com.bubbletea.commontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonTestApplication.class, args);
+    }
+
+}
