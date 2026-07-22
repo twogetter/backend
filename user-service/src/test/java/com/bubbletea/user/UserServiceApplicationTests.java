f643c5b21e945f911a781e7494c46ebe638a8aa6
유창민 <dnwn3295@gmail.com>
2026-07-22 14:33:42 +0900

    test(user): 애플리케이션 컨텍스트 테스트 복원

--- user-service/src/test/java/com/bubbletea/user/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/bubbletea/user/UserServiceApplicationTests.java
@@ -1,7 +1,9 @@
 package com.bubbletea.user;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class UserServiceApplicationTests {
 
     @Test
