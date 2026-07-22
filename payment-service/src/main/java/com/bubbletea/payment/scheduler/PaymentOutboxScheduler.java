ce8dfad02b16f4ffbb4d006485669e2f608cd907
jihoon <wlgnstls0413@naver.com>
2026-07-22 11:17:20 +0900

    chore

--- payment-service/src/main/java/com/bubbletea/payment/scheduler/PaymentOutboxScheduler.java
+++ payment-service/src/main/java/com/bubbletea/payment/scheduler/PaymentOutboxScheduler.java
@@ -1,19 +1,10 @@
 package com.bubbletea.payment.scheduler;
 
-import com.bubbletea.payment.entity.PaymentOutbox;
-import com.bubbletea.payment.entity.enums.OutboxStatus;
-import com.bubbletea.payment.processor.OutboxEventProcessor;
 import com.bubbletea.payment.processor.PaymentOutboxProcessor;
-import com.bubbletea.payment.repository.PaymentOutboxRepository;
-import java.time.LocalDateTime;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
-import org.springframework.transaction.support.TransactionTemplate;
 
 @Slf4j
 @Component
--- payment-service/src/main/java/com/bubbletea/payment/scheduler/PaymentRecoveryScheduler.java
+++ payment-service/src/main/java/com/bubbletea/payment/scheduler/PaymentRecoveryScheduler.java
@@ -1,22 +1,7 @@
 package com.bubbletea.payment.scheduler;
 
-import com.bubbletea.payment.entity.Payment;
-import com.bubbletea.payment.entity.PaymentCancel;
-import com.bubbletea.payment.entity.enums.CancelStatus;
-import com.bubbletea.payment.entity.enums.PaymentStatus;
-import com.bubbletea.payment.facade.PaymentCancelFacade;
-import com.bubbletea.payment.global.exception.PaymentErrorCode;
 import com.bubbletea.payment.processor.PaymentRecoverProcessor;
-import com.bubbletea.payment.repository.PaymentCancelRepository;
-import com.bubbletea.payment.repository.PaymentRepository;
-import com.bubbletea.payment.service.dto.PaymentCancelRequestDto;
-import com.bubbletea.payment.service.dto.TossStatusResponseDto;
-import com.bubbletea.payment.service.external.TossBrandpayApiClient;
-import com.bubbletea.payment.processor.PaymentPostProcessor;
-import java.time.LocalDateTime;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
