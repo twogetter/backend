9d5ff4123e00f432e550997961a7b1c9d7d0a84a
muncool39 <muncool39@gmail.com>
2026-07-25 04:02:47 +0900

    fix(product): EnableMongoAuditing 적용

--- product-service/src/main/java/com/bubbletea/product/ProductServiceApplication.java
+++ product-service/src/main/java/com/bubbletea/product/ProductServiceApplication.java
@@ -3,7 +3,10 @@ package com.bubbletea.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
+
+@EnableMongoAuditing
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ProductServiceApplication {
