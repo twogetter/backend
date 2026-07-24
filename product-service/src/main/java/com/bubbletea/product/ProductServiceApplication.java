d16ea90d52ccb19d3e8ba9dceb735e1deabaacc4
muncool39 <muncool39@gmail.com>
2026-07-25 04:17:07 +0900

    fix(product): Bean 정의 충돌 오류 해결
    - MongoAuditingTestConfig: 테스트용 설정 클래스 추가

--- product-service/src/main/java/com/bubbletea/product/ProductServiceApplication.java
+++ product-service/src/main/java/com/bubbletea/product/ProductServiceApplication.java
@@ -3,10 +3,8 @@ package com.bubbletea.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 
-@EnableMongoAuditing
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ProductServiceApplication {
---
+++ product-service/src/test/java/com/bubbletea/product/support/MongoAuditingTestConfig.java
@@ -0,0 +1,11 @@
+package com.bubbletea.product.support;
+
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@TestConfiguration
+@EnableMongoAuditing
+public class MongoAuditingTestConfig {
+
+}
