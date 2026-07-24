568ae770f66b3d66854e4a132d62c9b95230a1aa
muncool39 <muncool39@gmail.com>
2026-07-25 04:18:33 +0900

    test(product): infra ProductRepository 통합 테스트 코드 추가

--- product-service/src/main/java/com/bubbletea/product/infrastructure/mongo/product/ProductRepositoryImpl.java
+++ product-service/src/main/java/com/bubbletea/product/infrastructure/mongo/product/ProductRepositoryImpl.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-class ProductRepositoryImpl implements ProductRepository {
+public class ProductRepositoryImpl implements ProductRepository {
 
     private final ProductMongoRepository productMongoRepository;
     private final MongoTemplate mongoTemplate;
