6f99c7f2aeebf1f6b09c06b662a6d52ddbeaef20
윤수현 <ysh@knu.ac.kr>
2026-07-27 00:32:20 +0900

    test: ㅊ채팅방 및 메시지 송수신 API 통합 테스트 구현

--- chat-service/src/test/java/com/bubbletea/chat/ChatServiceApplicationTests.java
+++ chat-service/src/test/java/com/bubbletea/chat/ChatServiceApplicationTests.java
@@ -1,10 +1,11 @@
 package com.bubbletea.chat;
 
+import com.bubbletea.chat.support.ChatIntegrationTestSupport;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ChatServiceApplicationTests {
+class ChatServiceApplicationTests extends ChatIntegrationTestSupport {
 
     @Test
     void contextLoads() {
