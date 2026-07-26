fe5645a92f3f9df6fe6422d635f463727ef06d51
윤수현 <ysh@knu.ac.kr>
2026-07-27 00:31:07 +0900

    build: chat-service 통합 테스트 환경 및 기반 클래스 구성

---
+++ chat-service/src/test/java/com/bubbletea/chat/support/ChatIntegrationTestSupport.java
@@ -0,0 +1,30 @@
+package com.bubbletea.chat.support;
+
+import com.bubbletea.commontest.container.KafkaTestContainer;
+import com.bubbletea.commontest.container.PostgresTestContainer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers(disabledWithoutDocker = true)
+public abstract class ChatIntegrationTestSupport
+    implements PostgresTestContainer, KafkaTestContainer {
+
+  protected MockMvc mockMvc;
+  @Autowired
+  protected ObjectMapper objectMapper;
+  @Autowired
+  private WebApplicationContext context;
+
+  @BeforeEach
+  void mockMvcSetUp() {
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+  }
+
+}
